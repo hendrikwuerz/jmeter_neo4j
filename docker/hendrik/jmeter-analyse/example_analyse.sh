80668535ef64e7277db317d0ca573308ec966275
Hendrik Wuerz <hendrik.wuerz@ion2s.com>
2015-07-29 13:41:02 +0200

    - fixed analyse for new names

--- docker/hendrik/jmeter-analyse/example_analyse.sh
+++ docker/hendrik/jmeter-analyse/example_analyse.sh
@@ -2,6 +2,4 @@
 
 docker run \
 -v /home/hendrik/jmeter/logs:/logs \
--v /home/hendrik/jmeter/result:/result \
-hendrik/jmeter-analyse \
-/logs/results_tree_success.csv /logs/split/ /logs/min/
+hendrik/jmeter-analyse
---
+++ docker/hendrik/jmeter-analyse/install_analyse.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+chmod +x *.sh
+docker build -t hendrik/jmeter-analyse .
